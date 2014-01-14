b7a5b6da370c34b645b49102fcaa7b40f50442fa
francois_blanchard <contact@fblanchard.fr>
2014-01-14 21:43:18 +0100

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bimapi::Application.config.secret_key_base = 'd48c5f062222fc8d4fd769db1f220c37799ae0c8148b95bb178324e98f2bdcc4ecdb0e734404b4a28a5e5c2bd744e743befc63af564d511f4158fa4eaf028571'
